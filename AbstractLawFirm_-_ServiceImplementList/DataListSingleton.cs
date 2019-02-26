b8c6fac33bbf848ea65f16c5c265b82457dafd10
GUSTAW1R <yamantaw@mail.ru>
2019-02-26 11:24:48 +0400

    1/8

--- AbstractLawFirm_-_ServiceImplementList/DataListSingleton.cs
+++ AbstractLawFirm_-_ServiceImplementList/DataListSingleton.cs
@@ -15,6 +15,8 @@ namespace AbstractLawFirm___ServiceImplementList
         public List<Order> Orders { get; set; }
         public List<Documents> Documents { get; set; }
         public List<DocumentBlank> DocumentsComponent { get; set; }
+        public List<Archive> Archives { get; set; }
+        public List<ArchiveComponent> ArchiveComponents { get; set; }
         private DataListSingleton()
         {
             Customer = new List<Customer>();
@@ -22,6 +24,8 @@ namespace AbstractLawFirm___ServiceImplementList
             Orders = new List<Order>();
             Documents = new List<Documents>();
             DocumentsComponent = new List<DocumentBlank>();
+            Archives = new List<Archive>();
+            ArchiveComponents = new List<ArchiveComponent>();
         }
         public static DataListSingleton GetInstance()
         {
