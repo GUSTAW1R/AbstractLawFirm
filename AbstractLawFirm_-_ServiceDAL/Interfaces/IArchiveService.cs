2890894655a59f80621f3242b66dff02339bddd7
GUSTAW1R <yamantaw@mail.ru>
2019-02-26 11:26:30 +0400

    1/9

--- AbstractLawFirm_-_ServiceDAL/Interfaces/IArchiveService.cs
+++ AbstractLawFirm_-_ServiceDAL/Interfaces/IArchiveService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AbstractLawFirm___ServiceDAL.BindingModel;
+using AbstractLawFirm___ServiceDAL.ViewModel;
 
 namespace AbstractLawFirm___ServiceDAL.Interfaces
 {
-    class IArchiveService
+    public interface IArchiveService
     {
+        List<ArchiveViewModel> GetList();
+        ArchiveViewModel GetElement(int id);
+        void AddElement(ArchiveBindingModel model);
+        void UpdElement(ArchiveBindingModel model);
+        void DelElement(int id);
     }
 }
