fbc21b7c5bda489561446527e2ad4e5a38aa3d5f
GUSTAW1R <yamantaw@mail.ru>
2019-02-26 11:18:46 +0400

    1/5

--- AbstractLawFirm_-_ServiceDAL/BindingModel/ArchiveComponentBindingModel.cs
+++ AbstractLawFirm_-_ServiceDAL/BindingModel/ArchiveComponentBindingModel.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace AbstractLawFirm___ServiceDAL.BindingModel
 {
-    class ArchiveComponentBindingModel
+    public class ArchiveComponentBindingModel
     {
+        public int Id { get; set; }
+        public int ArchiveId { get; set; }
+        public int BlankId { get; set; }
+        public int Count { get; set; }
     }
 }
