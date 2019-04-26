889c22dc6b20419d02d58ab6c3331d781fcb7e0a
GUSTAW1R <yamantaw@mail.ru>
2019-04-26 20:43:40 +0400

    WPF_Step_1

---
+++ AbstractLawFirm_-_ViewWPF/FormAddNewBlank.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using AbstractLawFirm___ServiceDAL.Interfaces;
+using AbstractLawFirm___ServiceDAL.ViewModel;
+using Unity;
+
+namespace AbstractLawFirm___ViewWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для FormAddNewBlank.xaml
+    /// </summary>
+    public partial class FormAddNewBlank : Window
+    {
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+        private readonly IBlankService service;
+
+        public FormAddNewBlank(IBlankService service)
+        {
+            InitializeComponent();
+            this.service = service;
+        }
+
+        private void FormClients_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                List<BlankViewModel> list = service.GetList();
+                if (list != null)
+                {
+                    DataGridView.ItemsSource = list;
+                   
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormAddNewBlank>();
+            if (form.ShowDialog() == DialogResult)
+            {
+                LoadData();
+            }
+        }
+        private void buttonUpd_Click(object sender, EventArgs e)
+        {
+            if (DataGridView.SelectedRows.Count == 1)
+            {
+                var form = Container.Resolve<FormAddNewBlank>();
+                form.Id = Convert.ToInt32(DataGridView.SelectedRows[0].Cells[0].Value);
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    LoadData();
+                }
+            }
+        }
+        private void buttonDel_Click(object sender, EventArgs e)
+        {
+            if (DataGridView.SelectedRows.Count == 1)
+            {
+                if (MessageBox.Show("Удалить запись", "Вопрос", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    int id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                    try
+                    {
+                        service.DelElement(id);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonRef_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
